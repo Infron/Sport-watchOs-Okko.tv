8686e7ddd90aabe5d4fc5e747bfc9f9442202f33
ldkars <igra.22play@bk.ru>
2020-07-12 13:38:06 +0900

    cardSlider fix bug out range

--- Landmarks/WatchLandmarks Extension/ContentView.swift
+++ Landmarks/WatchLandmarks Extension/ContentView.swift
@@ -1,10 +1,3 @@
-/*
-See LICENSE folder for this sample’s licensing information.
-
-Abstract:
-A view that presents the contents of the watchOS app's root view.
-*/
-
 import SwiftUI
 
 struct MainCard: View {
@@ -30,13 +23,29 @@ struct MainCard: View {
 func initPaddingMap(count: Int)->[(Int)]
 {
     var updates: [Int] = []
-    for _ in 0...count-3 {
-        updates.append(20)
+    
+    if count >= 3 {
+        for _ in 0...count-3 {
+            updates.append(20)
+        }
+        
+        updates.append(10)
+        updates.append(0)
+        updates.append(0)
+    }else if count == 2{
+        for _ in 0...count-2 {
+            updates.append(10)
+        }
+        
+        updates.append(0)
+    }else if count == 1 {
+        for _ in 0...count-1 {
+            updates.append(20)
+        }
+        updates.append(10)
     }
     
-    updates.append(10)
-    updates.append(0)
-    updates.append(0)
+    
 
    return updates
 }
@@ -44,13 +53,28 @@ func initPaddingMap(count: Int)->[(Int)]
 func initPaddingBottomMap(count: Int)->[Int]
 {
     var updates: [Int] = []
-    for _ in 0...count-3 {
-        updates.append(-40)
-    }
     
-    updates.append(-20)
-    updates.append(0)
-    updates.append(0)
+    if count >= 3{
+        for _ in 0...count-3 {
+            updates.append(-40)
+        }
+        
+        updates.append(-20)
+        updates.append(0)
+        updates.append(0)
+    }else if count == 2{
+        for _ in 0...count-2 {
+            updates.append(-20)
+        }
+        
+        updates.append(0)
+    }else if count == 1{
+        for _ in 0...count-1 {
+            updates.append(-40)
+        }
+        
+        updates.append(-20)
+    }
     
    return updates
 }
@@ -75,27 +99,20 @@ func initX(count: Int)->[(CGFloat)]{
 
 
 struct cardSlider: View {
-    var userData: UserData
-    
     var count: Int = UserData().landmarks.count
     
     @State var x: [CGFloat] = initX(count: UserData().landmarks.count)
     
     @State var dragIsActivate: Bool = false
     
-    
-    
     @State var shadowMap: [Int] = initShadowMap()
     @State var paddingMap: [Int] = initPaddingMap(count: UserData().landmarks.count)
     @State var paddingBottomMap: [Int] = initPaddingBottomMap(count: UserData().landmarks.count)
     
-    
-    
     var body: some View {
-    
-        print(UserData().landmarks.count)
+        
     return ZStack{
-         ForEach(1..<count, id: \.self){
+         ForEach(0..<count, id: \.self){
          i in
             MainCard(index: i)
                  .offset(y: self.x[i])
@@ -106,75 +123,94 @@ struct cardSlider: View {
                      self.dragIsActivate = true
                      
                      if self.x[i] > 50{
-                         self.x[i] = 170
-                        
-                         self.paddingMap[i - 1] = 0
-                         self.paddingMap[i - 2] = 10
-                         self.paddingMap[i - 3] = 20
                          
-                         self.paddingBottomMap[i - 1] = 0
-                         self.paddingBottomMap[i - 2] = -20
-                         self.paddingBottomMap[i - 3] = -40
+                        if (i+1 == 1){
+                        }else{
+                            self.x[i] = 170
+                        }
+                        
+                        if(i+1 == 2){
+                            self.paddingMap[i - 1] = 0
+                            self.paddingBottomMap[i - 1] = 0
+                        }else if (i+1 == 3){
+                            self.paddingMap[i - 1] = 0
+                            self.paddingMap[i - 2] = 10
+                            
+                            self.paddingBottomMap[i - 1] = 0
+                            self.paddingBottomMap[i - 2] = -20
+                        }else if(i+1 > 3){
+                            self.paddingMap[i - 1] = 0
+                            self.paddingMap[i - 2] = 10
+                            self.paddingMap[i - 3] = 20
+                            
+                            self.paddingBottomMap[i - 1] = 0
+                            self.paddingBottomMap[i - 2] = -20
+                            self.paddingBottomMap[i - 3] = -40
+                        }
   
                      }else if self.x[i] < -50{
-                         self.x[i] = -170
+                            if (i+1 == 1){
+                            }else{
+                                self.x[i] = -170
+                            }
                         
-                         self.paddingMap[i - 1] = 0
-                         self.paddingMap[i - 2] = 10
-                         self.paddingMap[i - 3] = 20
-                         
-                         self.paddingBottomMap[i - 1] = 0
-                         self.paddingBottomMap[i - 2] = -20
-                         self.paddingBottomMap[i - 3] = -40
-                        
-                     }else{
-                         self.x[i] = value.translation.height
-                     }
-                 })
-                 
-                     .onEnded({
+                        if(i+1 == 2){
+                            self.paddingMap[i - 1] = 0
+                            self.paddingBottomMap[i - 1] = 0
+                        }else if (i+1 == 3){
+                            self.paddingMap[i - 1] = 0
+                            self.paddingMap[i - 2] = 10
+                            
+                            self.paddingBottomMap[i - 1] = 0
+                            self.paddingBottomMap[i - 2] = -20
+                        }else if(i+1 > 3){
+                            self.paddingMap[i - 1] = 0
+                            self.paddingMap[i - 2] = 10
+                            self.paddingMap[i - 3] = 20
+                            
+                            self.paddingBottomMap[i - 1] = 0
+                            self.paddingBottomMap[i - 2] = -20
+                            self.paddingBottomMap[i - 3] = -40
+                        }
+                        }else{
+                            self.x[i] = value.translation.height
+                        }
+                 }).onEnded({
                          (value) in
                          
                          self.dragIsActivate = false
                          
                          if self.x[i] == 170 || self.x[i] == -170 {
-                             
-                         }else{
+                            
+                         }
+                         else{
                              self.x[i] = 0
                          }
                  })))
                  .animation(.spring())
-                .padding(.bottom, CGFloat(self.paddingBottomMap[i]))
-                .shadow(radius: /*self.dragIsActivate ? CGFloat(self.shadowMap[i]) : 0*/ 8)
-            .padding(CGFloat(self.paddingMap[i]))
+                 .padding(.bottom, CGFloat(self.paddingBottomMap[i]))
+                 .shadow(radius: /*self.dragIsActivate ? CGFloat(self.shadowMap[i]) : 0*/ 8)
+                 .padding(CGFloat(self.paddingMap[i]))
          }
     
         }
-        
     }
-   
-    /*struct cardSlider_Previews: PreviewProvider {
-    static var previews: some View {
-        LandmarkList { WatchLandmarkDetail(landmark: $0) }
-            .environmentObject(UserData())
-        
-    }*/
-    
 }
 
 
 struct ContentView: View {
+    @State var scrollAmount = 0.0
     
     var body: some View {
-        cardSlider(userData: UserData())
+        cardSlider()
     }
 
 }
 
-/*struct ContentView_Previews: PreviewProvider {
+struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         LandmarkList { WatchLandmarkDetail(landmark: $0) }
             .environmentObject(UserData())
         
     }
-}*/
+}
